9c95ea4c0655747c6cbb4536e2ed9c4f42e4ddc3
Sandeep <sandeepsharma116@gmail.com>
2017-08-07 18:29:54 +0530

    First commit

--- app/build/generated/source/r/debug/com/example/android/accelerometerplay/R.java
+++ app/build/generated/source/r/debug/com/example/android/accelerometerplay/R.java
@@ -9,12 +9,49 @@ package com.example.android.accelerometerplay;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesColor=0x7f010003;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesData=0x7f010000;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesTitle=0x7f010002;
+        /** <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+         */
+        public static final int seriesType=0x7f010001;
     }
     public static final class drawable {
         public static final int ball=0x7f020000;
         public static final int wood=0x7f020001;
     }
     public static final class id {
+        public static final int button=0x7f060005;
+        public static final int button2=0x7f060007;
+        public static final int button3=0x7f060008;
+        public static final int graph=0x7f060006;
         public static final int rl=0x7f060000;
         public static final int textView1=0x7f060001;
         public static final int textView2=0x7f060002;
@@ -33,4 +70,91 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int menu_settings=0x7f050003;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a GraphViewXML.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #GraphViewXML_android_title android:title}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesColor com.example.android.accelerometerplay:seriesColor}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesData com.example.android.accelerometerplay:seriesData}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesTitle com.example.android.accelerometerplay:seriesTitle}</code></td><td></td></tr>
+           <tr><td><code>{@link #GraphViewXML_seriesType com.example.android.accelerometerplay:seriesType}</code></td><td></td></tr>
+           </table>
+           @see #GraphViewXML_android_title
+           @see #GraphViewXML_seriesColor
+           @see #GraphViewXML_seriesData
+           @see #GraphViewXML_seriesTitle
+           @see #GraphViewXML_seriesType
+         */
+        public static final int[] GraphViewXML = {
+            0x010101e1, 0x7f010000, 0x7f010001, 0x7f010002,
+            0x7f010003
+        };
+        /**
+          <p>This symbol is the offset where the {@link android.R.attr#title}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+          @attr name android:title
+        */
+        public static final int GraphViewXML_android_title = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.example.android.accelerometerplay.R.attr#seriesColor}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a color value, in the form of "<code>#<i>rgb</i></code>", "<code>#<i>argb</i></code>",
+"<code>#<i>rrggbb</i></code>", or "<code>#<i>aarrggbb</i></code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.android.accelerometerplay:seriesColor
+        */
+        public static final int GraphViewXML_seriesColor = 4;
+        /**
+          <p>This symbol is the offset where the {@link com.example.android.accelerometerplay.R.attr#seriesData}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.android.accelerometerplay:seriesData
+        */
+        public static final int GraphViewXML_seriesData = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.example.android.accelerometerplay.R.attr#seriesTitle}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.android.accelerometerplay:seriesTitle
+        */
+        public static final int GraphViewXML_seriesTitle = 3;
+        /**
+          <p>This symbol is the offset where the {@link com.example.android.accelerometerplay.R.attr#seriesType}
+          attribute's value can be found in the {@link #GraphViewXML} array.
+
+
+          <p>Must be a string value, using '\\;' to escape characters such as '\\n' or '\\uxxxx' for a unicode character.
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name com.example.android.accelerometerplay:seriesType
+        */
+        public static final int GraphViewXML_seriesType = 2;
+    };
 }
--- app/src/main/java/com/example/android/accelerometerplay/MainActivity.java
+++ app/src/main/java/com/example/android/accelerometerplay/MainActivity.java
@@ -7,27 +7,42 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.text.Html;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Button;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+
 public class MainActivity extends Activity implements SensorEventListener {
+    GraphView graph;
+    ArrayList data= new ArrayList();
+
 
     private SensorManager sensorManager;
+    boolean D=false;
 
     TextView x;
     TextView y;
     TextView z;
 
     String sx, sy, sz;
+    LineGraphSeries<DataPoint> series = new LineGraphSeries<>();
+    GraphView graph1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
+
         x = (TextView) findViewById (R.id.textView2);
         y = (TextView) findViewById (R.id.textView3);
         z = (TextView) findViewById (R.id.textView4);
+        graph1 = (GraphView) findViewById(R.id.graph);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
@@ -52,6 +67,9 @@ public class MainActivity extends Activity implements SensorEventListener {
             float xVal = event.values[0];
             float yVal = event.values[1];
             float zVal = event.values[2];
+            if (D==true){
+                data.add(zVal);
+            }
 
             sx = "X Value : <font color = '#800080'> " + xVal + "</font>";
             sy = "Y Value : <font color = '#800080'> " + yVal + "</font>";
@@ -61,5 +79,26 @@ public class MainActivity extends Activity implements SensorEventListener {
             y.setText(Html.fromHtml(sy));
             z.setText(Html.fromHtml(sz));
         }
+    }
+    public void onstart(View v){
+        D=true;
+        data.clear();
+
+    }
+    public void onstop(View v){
+        D=false;
+
+
+    }
+    public void onplot(View v){
+        int E=data.size();
+        for(int i=0;i<E;i++){
+            float F=(float)data.get(i);
+            series.appendData(new DataPoint(i,F),true,E,false);
+
+
+        }
+        graph1.addSeries(series);
+
     }
 }
